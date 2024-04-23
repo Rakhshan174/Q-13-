bb4a1a577a243e13d51fa1a3c3a4010dc77cb2f5
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:58:29 -0700

    Q 13 TS/JS for 45 assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+// Your Own Array: Think of your favoourite mode of transportation,Such as a motorcyle or a car, and make a list
+// that store several examplee. Use ypur llist to print a series of statement about these items, Such as 
+//" I Would Like to own a Honda motorcycle."
+var transportation = ["Honda Motorcyle", "Ausi", "Honda City"];
+transportation.map(function (items) { return console.log("I would like to own a ".concat(items)); });
---
+++ main.ts
@@ -0,0 +1,7 @@
+// Your Own Array: Think of your favoourite mode of transportation,Such as a motorcyle or a car, and make a list
+// that store several examplee. Use ypur llist to print a series of statement about these items, Such as 
+//" I Would Like to own a Honda motorcycle."
+
+
+let transportation: string [] = ["Honda Motorcyle", "Ausi", "Honda City"];
+transportation.map((items)=> console.log(`I would like to own a ${items}`));
